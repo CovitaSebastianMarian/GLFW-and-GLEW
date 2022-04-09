dc961b13448f1c38b9408cede6a2702b8b06570e
Sebastian <88426759+CovitaSebastianMarian@users.noreply.github.com>
2022-04-09 19:55:52 +0300

    Update main.cpp

--- 8. Ortho translate/main.cpp
+++ 8. Ortho translate/main.cpp
@@ -22,6 +22,11 @@ using namespace std;
 
 
 double wwindowsize = 1200 , hwindowsize = 700;
+double xmouse, ymouse, xpos, ypos;
+double zoom = 0.0;
+
+
+
 
 void reshape(GLFWwindow* window)
 {
@@ -36,15 +41,44 @@ void key(GLFWwindow* window, int key, int scanmode, int state, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && state == GLFW_PRESS) exit(0);
 }
-
+void cursor(GLFWwindow* window, double x, double y) {
+    xmouse = x;
+    ymouse = y;
+}
+bool ok2 = false;
+void mouse(GLFWwindow* window, int button, int action, int mods) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+       if (!ok2) {
+            xpos = xmouse;
+            ypos = ymouse;
+            ok2 = true;
+       }
+    }
+    else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
+        ok2 = false;
+    }
+}
+bool ok = false;
+void scrollfunc(GLFWwindow* window, double x, double y)
+{
+    if (y == 1)
+    {
+        ok = true;
+        zoom = 0.4;
+    }
+    else if (y == -1)
+    {
+        ok = true;
+        zoom = -0.4;
+    }
+    reshape(window);
+}
 int main()
 {
     
     if (!glfwInit()) {
         return -1;
     }
-
-
 
     GLFWwindow* window = glfwCreateWindow(wwindowsize, hwindowsize, "GLFW and GLEW", NULL, NULL);
     if (!window)
@@ -63,20 +97,23 @@ int main()
         return -1;
     }
 
+    
+
+
+    glfwSetKeyCallback(window, key);
+    glfwSetCursorPosCallback(window, cursor);
+    glfwSetScrollCallback(window, scrollfunc);
+    glfwSetMouseButtonCallback(window, mouse);
 
 
 
     GLCall(glEnable(GL_BLEND));
     GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-    glfwSetKeyCallback(window, key);
-    //reshape(window);
-
-    
-
-
     ShaderProgramSource source = ParseShader("Basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+
+
 
     unsigned int VAO, VBO;
 
@@ -103,7 +140,7 @@ int main()
 
     GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0));
     GLCall(glEnableVertexAttribArray(0));
-
+    
     GLCall(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float))));
     GLCall(glEnableVertexAttribArray(1));
 
@@ -112,49 +149,42 @@ int main()
 
 
     int width, height, nrChannels; 
-    stbi_set_flip_vertically_on_load(true);
-    unsigned char* data = stbi_load("image.jpg", &width, &height, &nrChannels, 0);
-
-    GLuint texture;
+    //stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load("grass.jpg", &width, &height, &nrChannels, 0);
+   
+    GLuint texture; 
     glGenTextures(1, &texture);
-    glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
  
     stbi_image_free(data);
 
-    GLCall(glUseProgram(shader));
-
-    glm::mat4 transform(1.0);
+    
+    
+    glm::mat4 transform(1.0); 
     transform = glm::ortho(0.0, wwindowsize, hwindowsize, 0.0, -1.0, 1.0);
-    transform = glm::translate(transform, glm::vec3(wwindowsize / 2, hwindowsize/2, 0.0));
-    transform = glm::scale(transform, glm::vec3(1, 1, 1));
+    transform = glm::translate(transform, glm::vec3(wwindowsize/2, hwindowsize/2, 0.0));
+    transform = glm::rotate(transform, (float)glm::radians(0.0), glm::vec3(0.0, 0.0, 1.0));
+    transform = glm::scale(transform, glm::vec3(1.0, 1.0, 1.0));
+
+    GLCall(glUseProgram(shader));
 
     int location = glGetUniformLocation(shader, "Matrix");
     GLCall(glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(transform)));
-  
-    /*
-    * ###############################################################
-    * !!! mai intai GLCall(glUseProgram(shader)); si dupa atribuim valori datelor uniform din shader !!!
-    * ################################################################
-    * GL_LINEAR = nu ai pixeli, culorile sunt linii smooth
-    * GL_NEAREST = ai imaginea facuta din pixeli
-    * #################################################################
-    */
-
-
-
-
 
     
-    
+
+
 
     while (!glfwWindowShouldClose(window))
     {
@@ -165,6 +195,18 @@ int main()
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
+        if (ok)
+        {
+            transform = glm::scale(transform, glm::vec3(1.0 + zoom, 1.0 + zoom, 1.0 + zoom));
+            GLCall(glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(transform)));
+            ok = false;
+        }
+        if (ok2) {
+            transform = glm::translate(transform, glm::vec3(xmouse - xpos, ymouse - ypos, 0.0));
+            xpos = xmouse;
+            ypos = ymouse;
+            GLCall(glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(transform)));
+        }
         glBindVertexArray(VAO);
         GLCall(glDrawArrays(GL_POLYGON, 4, 4));
 
