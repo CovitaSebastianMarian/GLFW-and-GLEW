fabbc39d0c2f264d67ec3e9a926698f9f965be9b
Sebastian <88426759+CovitaSebastianMarian@users.noreply.github.com>
2022-04-17 16:40:25 +0300

    Update main.cpp

--- 10. Object class/main.cpp
+++ 10. Object class/main.cpp
@@ -1,125 +1,134 @@
-#pragma once
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <fstream>
+#include <Windows.h>
 #include <iostream>
-#include "stb_image.h"
-#include <vector>
+#include <string>
+#include <sstream>
+//#include "stb_image.h"
+#include <thread>
 #include "Shader.h"
 #include "error.h"
+#include "Object.h"
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_access.hpp>
 
+#define PI 3.14159265359
+
+
 using namespace std;
 
-class Object {
-private:
-	unsigned int VAO, VBO, texture, shader;
-	int location;
-	glm::mat4 PT = glm::mat4(1.0);
-	glm::mat4 perspective = glm::mat4(1.0);
-	glm::mat4 camera = glm::mat4(1.0);
-	glm::mat4 ortho = glm::mat4(1.0);
-	glm::mat4 translate = glm::mat4(1.0);
-	glm::mat4 rotate = glm::mat4(1.0);
-	glm::mat4 scale = glm::mat4(1.0);
-public:
-	Object() {}
-	~Object() {
-		glDeleteVertexArrays(1, &VAO);
-		glDeleteBuffers(1, &VBO);
-		glDeleteTextures(1, &texture);
-		glDeleteProgram(shader);
-	}
-	void Init(float positions[], const int size) {
-		glDeleteVertexArrays(1, &VAO);
-		glDeleteBuffers(1, &VBO);
 
-		glGenVertexArrays(1, &VAO);
-		glGenBuffers(1, &VBO);
+double wwindowsize = 1200 , hwindowsize = 700;
+double xpos = 100, ypos = 100;
+float p = 0;
+void reshape(GLFWwindow* window)
+{
+    int w, h;
+    glfwGetWindowSize(window, &w, &h);
+    wwindowsize = w;
+    hwindowsize = h;
+    glViewport(0, 0, wwindowsize, hwindowsize);
+}
 
-		glBindVertexArray(VAO);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, size, positions, GL_STATIC_DRAW);
+void key(GLFWwindow* window, int key, int scanmode, int state, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && state == GLFW_PRESS) exit(0);
+    if (key == GLFW_KEY_W && state == GLFW_PRESS) p+=0.5;
+    if (key == GLFW_KEY_S && state == GLFW_PRESS) p-=0.5;
+}
 
-	};
-	void VertexAttribpointer(int index, int start, int count, int jump, int type) {
-		glVertexAttribPointer(index, count, type, GL_FALSE, jump * sizeof(float), (void*)(start * sizeof(float)));
-		glEnableVertexAttribArray(index);
-	}
-	void Shader(const char * filename) {
-		ShaderProgramSource source = ParseShader(filename);
-		shader = CreateShader(source.VertexSource, source.FragmentSource);
-		glUseProgram(shader);
 
-		location = glGetUniformLocation(shader, "Matrix");
 
-		PT = perspective * camera * (ortho * translate * rotate * scale);
-	}
-	void Texture(const char * filename, bool flip) {
-		glDeleteTextures(1, &texture);
+int main()
+{
+    
+    if (!glfwInit()) {
+        return -1;
+    }
 
-		int width, height, nrChannels;
-		stbi_set_flip_vertically_on_load(flip);
-		unsigned char* data = stbi_load(filename, &width, &height, &nrChannels, 0);
+    GLFWwindow* window = glfwCreateWindow(wwindowsize, hwindowsize, "GLFW and GLEW", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    if (glewInit() != GLEW_OK) {    
+        cout << "\033[3;40;31m[Error: glew =try> move glewinit() after window init]" << endl;
+        return -1;
+    }
+    glfwSetKeyCallback(window, key);
+    
+    GLCall(glEnable(GL_POINT_SMOOTH));
+    GLCall(glHint(GL_POINT_SMOOTH_HINT, GL_NICEST));
+    GLCall(glEnable(GL_LINE_SMOOTH));
+    GLCall(glHint(GL_LINE_SMOOTH_HINT, GL_NICEST));
+    GLCall(glEnable(GL_POLYGON_SMOOTH));
+    GLCall(glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST));
+    GLCall(glEnable(GL_BLEND));
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+    GLCall(glEnable(GL_DEPTH_TEST));
 
-		glGenTextures(1, &texture);
-		glBindTexture(GL_TEXTURE_2D, texture);
 
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
+    float sze = 100;
+    float positions[] = {
+        //position          color      texture coord
+        -sze, -sze, 0,      1,0,0,     0,1,
+        sze, -sze, 0,       0,1,0,     1,1,
+        sze,sze,0,          0,0,1,     1,0,
+        -sze, sze, 0,       1,1,0,     0,0,
+    };
+    Object obj;
+    obj.Init(positions, sizeof(positions));
+    obj.VertexAttribpointer(0, 0, 3, 8, GL_FLOAT);
+    obj.VertexAttribpointer(1, 3, 3, 8, GL_FLOAT);
+    obj.VertexAttribpointer(2, 6, 2, 8, GL_FLOAT);
+    obj.Shader("Basic.shader");
+    obj.Texture("image.jpg", true);
+    obj.Perspective(glm::radians(45.0), 1.0, 0.1, 100.0);
+    obj.Camera(glm::vec3(0, 0.5, 0.5), glm::vec3(0, 0, -0.5), glm::vec3(0, 1, 0));
+    obj.Ortho(0.0, wwindowsize, hwindowsize, 0.0, 100.0, -100.0);
+    obj.Translate(glm::vec3(wwindowsize / 2, hwindowsize / 2, 0), false);
+    obj.Rotate(glm::radians(45.0), glm::vec3(0, 0, 1), false);
+    obj.Scale(glm::vec3(1,1,1), false);
+    
+    Object brick;
+    brick.Init(positions, sizeof(positions));
+    brick.VertexAttribpointer(0, 0, 3, 8, GL_FLOAT);
+    brick.VertexAttribpointer(1, 3, 3, 8, GL_FLOAT);
+    brick.VertexAttribpointer(2, 6, 2, 8, GL_FLOAT);
+    brick.Shader("Basic.shader");
+    brick.Texture("brick.jpg", true);
+    brick.Perspective(glm::radians(45.0), 1.0, 0.1, 100.0);
+    brick.Camera(glm::vec3(0, 0.5, 0.5), glm::vec3(0, 0, -0.5), glm::vec3(0, 1, 0));
+    brick.Ortho(0.0, wwindowsize, hwindowsize, 0.0, 100.0, -100.0);
+    brick.Translate(glm::vec3(wwindowsize /2, hwindowsize /2, 0.1), false);
+    brick.Rotate(glm::radians(45.0), glm::vec3(0, 0, 1), false);
+    brick.Scale(glm::vec3(0.5, 0.5, 0.5), false);
 
-		stbi_image_free(data);
-	}
-	void Perspective(double fovy, double aspect, double zNear, double zFar) {
-		perspective = glm::mat4(1.0);
-		perspective = glm::perspective(fovy, aspect, zNear, zFar);
-		//glm::mat4 PT = glm::mat4(1.0);
-		PT = perspective * camera * (ortho * translate * rotate * scale);
-	}
-	void Camera(glm::vec3 eye, glm::vec3 center, glm::vec3 up) {
-		camera = glm::mat4(1.0);
-		camera = glm::lookAt(eye, center, up);
-		//glm::mat4 PT = glm::mat4(1.0);
-		PT = perspective * camera * (ortho * translate * rotate * scale);
-	}
-	void Ortho(double left, double right, double bottmom, double top, double zNear, double zFar) {
-		ortho = glm::mat4(1.0);
-		ortho = glm::ortho(left, right, bottmom, top, zNear, zFar);
-		//glm::mat4 PT = glm::mat4(1.0);
-		PT = perspective * camera * (ortho * translate * rotate * scale);
-	}
-	void Translate(glm::vec3 pos, bool repeat) {
-		if(!repeat) translate = glm::mat4(1.0);
-		translate = glm::translate(translate, pos);
-		//glm::mat4 PT = glm::mat4(1.0);
-		PT = perspective * camera * (ortho * translate * rotate * scale);
-	}
-	void Scale(glm::vec3 dim, bool repeat) {
-		if (!repeat) scale = glm::mat4(1.0);
-		scale = glm::scale(scale, dim);
-		//glm::mat4 PT = glm::mat4(1.0);
-		PT = perspective * camera * (ortho * translate * rotate * scale);
-	}
-	void Rotate(float angle, glm::vec3 dir, bool repeat) {
-		if(!repeat)rotate = glm::mat4(1.0);
-		rotate = glm::rotate(rotate, angle, dir);
-		//glm::mat4 PT = glm::mat4(1.0);
-		PT = perspective * camera * (ortho * translate * rotate * scale);
-	}
 
-	void Bind() {
-		glUseProgram(shader);
-		glBindTexture(GL_TEXTURE_2D, texture);
-		glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(PT));
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_QUADS, 0, 4);
-	}
-};
+
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        reshape(window);
+
+        obj.Bind();
+        brick.Bind();
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        this_thread::sleep_for(chrono::microseconds(1000 / 60));
+    }
+
+    glfwTerminate();
+    return 0;
+}
