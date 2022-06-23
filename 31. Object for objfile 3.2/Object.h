3796095a7d2a02912b6b92464df374c2c2be2667
Sebastian <88426759+CovitaSebastianMarian@users.noreply.github.com>
2022-06-23 14:42:50 +0300

    Update Object.h

--- 31. Object for objfile 3.2/Object.h
+++ 31. Object for objfile 3.2/Object.h
@@ -55,90 +55,69 @@ using namespace std;
 ##############################################################################*/
 
 
-class Object {
+class Object{
 protected:
 	unsigned int VAO, VBO, EBO;
 	int location;
-	long long nr = 0;
 public:
 	vector<float> Vertices;
 	vector<unsigned int> Indices;
-
 	unsigned int texture, shader;
 	glm::mat4 Matrix = glm::mat4(1.0);
 	glm::mat4 perspective = glm::mat4(1.0);
-	glm::mat4 lookat = glm::mat4(1.0);
+	glm::mat4 camera = glm::mat4(1.0);
 	glm::mat4 ortho = glm::mat4(1.0);
 	glm::mat4 translate = glm::mat4(1.0);
 	glm::mat4 rotate = glm::mat4(1.0);
 	glm::mat4 scale = glm::mat4(1.0);
 
 	Object() {}
-
-	void LoadModelTriangles(const char* modelname) {
-		FILE *model = fopen(modelname, "r");
-
-		vector<glm::vec3> vertices;
-		vector<glm::vec2> vtextures;
-		vector<glm::vec3> vnormals;
-
-		bool newmodel = true;
-
-		if (model == NULL) {
-			cout << "ERROR\n";
-		}
-		char val[128];
-		while (true) {
-			int res = fscanf(model, "%s", val);
-			if (res == EOF) break;
-
-			if (strcmp(val, "v") == 0) {
-				glm::vec3 v;
-				fscanf(model, "%f %f %f\n", &v.x, &v.y, &v.z);
-				vertices.push_back(v);
-			}
-			else if (strcmp(val, "vt") == 0) {
-				glm::vec2 vt;
-				fscanf(model, "%f %f\n", &vt.x, &vt.y);
-				vtextures.push_back(vt);
-			}
-			else if (strcmp(val, "vn") == 0) {
-				glm::vec3 vn;
-				fscanf(model, "%f %f %f\n", &vn.x, &vn.y, &vn.z);
-				vnormals.push_back(vn);
-			}
-			else if (strcmp(val, "f") == 0) {
-				unsigned int v[3], vt[3], vn[3];
-				fscanf(model, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &v[0], &vt[0], &vn[0], &v[1], &vt[1], &vn[1], &v[2], &vt[2], &vn[2]);
-
-				for (int i = 0; i < 3; ++i) {
-					Indices.push_back(nr++);
-					glm::vec3 vv = vertices[v[i] - 1];
-					glm::vec2 vtt = vtextures[vt[i] - 1];
-					glm::vec3 vnn = vnormals[vn[i] - 1];
-					Vertices.push_back(vv.x);
-					Vertices.push_back(vv.y);
-					Vertices.push_back(vv.z);
-					Vertices.push_back(vtt.x);
-					Vertices.push_back(vtt.y);
-					Vertices.push_back(vnn.x);
-					Vertices.push_back(vnn.y);
-					Vertices.push_back(vnn.z);
-				}
-			}
-
-		}
-
-
-		vertices.clear();
-		vtextures.clear();
-		vnormals.clear();
-		fclose(model);
+	void Destruct() {
+		GLCallReturn(glDeleteVertexArrays(1, &VAO));
+		GLCallReturn(glDeleteBuffers(1, &VBO));
+		GLCallReturn(glDeleteTextures(1, &texture));
+		GLCallReturn(glDeleteProgram(shader));
 	}
+	void Init(float positions[], const int size, int usage) {
+		GLCallReturn(glDeleteVertexArrays(1, &VAO));
+		GLCallReturn(glDeleteBuffers(1, &VBO));
 
-	
+		GLCallReturn(glGenVertexArrays(1, &VAO));
+		GLCallReturn(glGenBuffers(1, &VBO));
+
+		GLCallReturn(glBindVertexArray(VAO));
+		GLCallReturn(glBindBuffer(GL_ARRAY_BUFFER, VBO));
+		GLCallReturn(glBufferData(GL_ARRAY_BUFFER, size, positions, usage));
+	};
+	void DynamicInit(int usage) {
+		GLCallReturn(glDeleteVertexArrays(1, &VAO));
+		GLCallReturn(glDeleteBuffers(1, &VBO));
+
+		GLCallReturn(glGenVertexArrays(1, &VAO));
+		GLCallReturn(glGenBuffers(1, &VBO));
+
+		GLCallReturn(glBindVertexArray(VAO));
+		GLCallReturn(glBindBuffer(GL_ARRAY_BUFFER, VBO));
+		GLCallReturn(glBufferData(GL_ARRAY_BUFFER, Vertices.size() * sizeof(float), Vertices.data(), usage));
+
+	};
+	void Init(float positions[], const int size, unsigned int indices[], const int sizeind, int usage) {
+		GLCallReturn(glDeleteVertexArrays(1, &VAO));
+		GLCallReturn(glDeleteBuffers(1, &VBO));
+		GLCallReturn(glDeleteBuffers(1, &EBO));
+
+		GLCallReturn(glGenVertexArrays(1, &VAO));
+		GLCallReturn(glGenBuffers(1, &VBO));
+		GLCallReturn(glGenBuffers(1, &EBO));
+
+		GLCallReturn(glBindVertexArray(VAO));
+		GLCallReturn(glBindBuffer(GL_ARRAY_BUFFER, VBO));
+		GLCallReturn(glBufferData(GL_ARRAY_BUFFER, size, positions, usage));
+
+		GLCallReturn(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO));
+		GLCallReturn(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeind, indices, usage));
+	};
 	void Config(int usage) {
-
 		GLCallReturn(glDeleteVertexArrays(1, &VAO));
 		GLCallReturn(glDeleteBuffers(1, &VBO));
 		GLCallReturn(glDeleteBuffers(1, &EBO));
@@ -152,39 +131,43 @@ public:
 		GLCallReturn(glBufferData(GL_ARRAY_BUFFER, Vertices.size() * sizeof(float), Vertices.data(), usage));
 
 		GLCallReturn(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO));
-		GLCallReturn(glBufferData(GL_ELEMENT_ARRAY_BUFFER, Indices.size() * sizeof(unsigned int), Indices.data(), usage));
+		GLCallReturn(glBufferData(GL_ELEMENT_ARRAY_BUFFER, Indices.size() * sizeof(unsigned int), Indices.data(),  usage));
+	};
+	void Reload(float positions[], const int size) {
+		GLCallReturn(glBindVertexArray(VAO));
+		GLCallReturn(glBindBuffer(GL_ARRAY_BUFFER, VBO));
+		GLCallReturn(glBufferData(GL_ARRAY_BUFFER, size, positions, GL_STATIC_DRAW));
 	}
+	void Reload(float positions[], const int size, unsigned int indices[], const int sizeind) {
+		GLCallReturn(glBindVertexArray(VAO));
+		GLCallReturn(glBindBuffer(GL_ARRAY_BUFFER, VBO));
+		GLCallReturn(glBufferData(GL_ARRAY_BUFFER, size, positions, GL_STATIC_DRAW));
 
-	void Destruct() {
-		GLCallReturn(glDeleteVertexArrays(1, &VAO));
-		GLCallReturn(glDeleteBuffers(1, &VBO));
-		GLCallReturn(glDeleteBuffers(1, &EBO));
-		GLCallReturn(glDeleteTextures(1, &texture));
-		GLCallReturn(glDeleteProgram(shader));
+		GLCallReturn(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO));
+		GLCallReturn(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeind, indices, GL_STATIC_DRAW));
 	}
-
 	void LoadMatrix() {
-		Matrix = perspective * lookat * (ortho * translate * rotate * scale);
+		Matrix = perspective * camera * (ortho * translate * rotate * scale);
 	}
-	void VertexAttribpointer(int index, int start, int count, int jump, int value) {
+	void VertexAttribpointer(int index, int start, int count, int jump, int type) {
+		GLCallReturn(glVertexAttribPointer(index, count, type, GL_FALSE, jump * sizeof(float), (void*)(start * sizeof(float))));
 		GLCallReturn(glEnableVertexAttribArray(index));
-		GLCallReturn(glVertexAttribPointer(index, count, value, GL_FALSE, jump * sizeof(float), (void*)(start * sizeof(float))));
 	}
-	void Shader(string filename) {
+	void Shader(const char* filename) {
 		ShaderProgramSource source = ParseShader(filename);
 		shader = CreateShader(source.VertexSource, source.FragmentSource);
 		//GLCallReturn(glUseProgram(shader));
 
 		location = glGetUniformLocation(shader, "Matrix");
 
-		Matrix = perspective * lookat * (ortho * translate * rotate * scale); // LoadMatrix();
+		Matrix = perspective * camera * (ortho * translate * rotate * scale); // LoadMatrix();
 	}
-	void Texture(string filename, int format, bool flip, float WRAP_S_param = GL_CLAMP_TO_EDGE, float WRAP_T_param = GL_CLAMP_TO_EDGE, float MIN_FILTER_param = GL_LINEAR_MIPMAP_LINEAR, float  MAG_FILTER_param = GL_LINEAR_MIPMAP_LINEAR) {
+	void Texture(const char* filename, int format, bool flip, float WRAP_S_param = GL_CLAMP_TO_EDGE, float WRAP_T_param = GL_CLAMP_TO_EDGE, float MIN_FILTER_param = GL_LINEAR_MIPMAP_LINEAR, float  MAG_FILTER_param = GL_LINEAR_MIPMAP_LINEAR) {
 		GLCallReturn(glDeleteTextures(1, &texture));
 
 		int width, height, nrChannels;
 		stbi_set_flip_vertically_on_load(flip);
-		unsigned char* data = stbi_load(filename.c_str(), &width, &height, &nrChannels, 0);
+		unsigned char* data = stbi_load(filename, &width, &height, &nrChannels, 0);
 
 		if (!data) {
 			cout << "\033[3;40;31m[Error::Texture: " << filename << " we can't open it]\033[0m" << endl;
@@ -216,34 +199,34 @@ public:
 	void Perspective(double fovy, double aspect, double zNear, double zFar) {
 		perspective = glm::mat4(1.0);
 		perspective = glm::perspective(fovy, aspect, zNear, zFar);
-		Matrix = perspective * lookat * (ortho * translate * rotate * scale);
+		Matrix = perspective * camera * (ortho * translate * rotate * scale);
 	}
 	void LookAt(glm::vec3 eye, glm::vec3 center, glm::vec3 up) {
-		lookat = glm::mat4(1.0);
-		lookat = glm::lookAt(eye, center, up);
-		Matrix = perspective * lookat * (ortho * translate * rotate * scale);
+		camera = glm::mat4(1.0);
+		camera = glm::lookAt(eye, center, up);
+		Matrix = perspective * camera * (ortho * translate * rotate * scale);
 	}
 	void Ortho(double left, double right, double bottmom, double top, double zNear, double zFar) {
 		ortho = glm::mat4(1.0);
 		ortho = glm::ortho(left, right, bottmom, top, zNear, zFar);
-		Matrix = perspective * lookat * (ortho * translate * rotate * scale);
+		Matrix = perspective * camera * (ortho * translate * rotate * scale);
 	}
 	void Translate(glm::vec3 pos, bool repeat) {
 		if (!repeat) translate = glm::mat4(1.0);
 		translate = glm::translate(translate, pos);
-		Matrix = perspective * lookat * (ortho * translate * rotate * scale);
+		Matrix = perspective * camera * (ortho * translate * rotate * scale);
 	}
 	void Scale(glm::vec3 dim, bool repeat) {
 		if (!repeat) scale = glm::mat4(1.0);
 		scale = glm::scale(scale, dim);
-		Matrix = perspective * lookat * (ortho * translate * rotate * scale);
+		Matrix = perspective * camera * (ortho * translate * rotate * scale);
 	}
 	void Rotate(float angle, glm::vec3 dir, bool keep) {
 		if (!keep) {
 			rotate = glm::mat4(1.0);
 		}
 		rotate = glm::rotate(rotate, angle, dir);
-		Matrix = perspective * lookat * (ortho * translate * rotate * scale);
+		Matrix = perspective * camera * (ortho * translate * rotate * scale);
 	}
 	void Bind(int mode, int start, int count) {
 		GLCallReturn(glUseProgram(shader));
@@ -251,6 +234,14 @@ public:
 		GLCallReturn(glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(Matrix)));
 		GLCallReturn(glBindVertexArray(VAO));
 		GLCallReturn(glDrawArrays(mode, start, count));
+		GLCallReturn(glBindVertexArray(0));
+	}
+	void Bind(int mode, int count, int type, const void* indices) {
+		GLCallReturn(glUseProgram(shader));
+		GLCallReturn(glBindTexture(GL_TEXTURE_2D, texture));
+		GLCallReturn(glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(Matrix)));
+		GLCallReturn(glBindVertexArray(VAO));
+		GLCallReturn(glDrawElements(mode, count, type, indices));
 		GLCallReturn(glBindVertexArray(0));
 	}
 	void Bind(int mode) {
@@ -275,7 +266,6 @@ private:
 		vector<unsigned int> Indices;
 		string texture;
 	};
-	
 	void Init() {
 		string texture;
 		if (Textures.size() != 0) {
@@ -320,7 +310,7 @@ public:
 			int res = fscanf(objmodel, "%s", val);
 			if (res == EOF) break;
 
-			if (strcmp(val, "g") == 0 && newobject) {
+			if (strcmp(val, "usemtl") == 0 && newobject) {
 				Init();
 				cnt = 0;
 				Indices.clear();
@@ -372,7 +362,6 @@ public:
 		Init();
 		fclose(objmodel);
 	}
-
 	void OpenFile(const char * modelname) {
 		FILE* objfile = fopen(modelname, "r");
 
@@ -423,5 +412,6 @@ public:
 		fclose(objfile);
 	}
 };
+
 #endif
 #endif // STB_IMAGE_IMPLEMENTATION
